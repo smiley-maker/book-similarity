a98c46b05eef2c67eab64bfc1d891374d17771bf
Jordan Sinclair <jordan.sinclair@du.edu>
2022-12-26 13:36:42 -0700

    Added search functionality

--- similarity.py
+++ similarity.py
@@ -3,21 +3,20 @@ import pandas as pd
 import re
 import nltk
 from nltk.corpus import stopwords
-from nltk import word_tokenize
 from nltk.stem.porter import PorterStemmer
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer
 import numpy as np
 import gensim
 from gensim.models import LdaModel
 from gensim import models, corpora, similarities
 import matplotlib.pyplot as plt
 from scipy.stats import entropy
+import urllib.request
 nltk.download('punkt')
 nltk.download('stopwords')
 stop_words = set(stopwords.words('english'))
 import warnings
 warnings.filterwarnings("ignore")
+from PIL import Image
 
 #Function to preprocess the data (not much was needed here, but 
 # might be more involved with a different dataset)
@@ -26,6 +25,7 @@ def preprocess(data):
     #so that I could view the original summaries again later
     data["summary"] = data["description"]
     data.drop("book_id_mapping", inplace=True, axis=1) #Not needed for my application
+    data["title"] = [t.lower() for t in df["title"]]
 
 def clean_text(par):
     par = par.lower() #Changes everything to lowercase
@@ -38,24 +38,25 @@ def clean_text(par):
     return par #returns the modified paragraph
 
 def remove_stop_words(par):
-    return [word for word in par if word not in stop_words]
+    return [word for word in par if word not in stop_words] #Removes the stop words from NLTK. 
 
 def stem_words(par):
-    porter = PorterStemmer()
-    return [porter.stem(word) for word in par if len(word) > 1]
+    porter = PorterStemmer() 
+    return [porter.stem(word) for word in par if len(word) > 1] #Uses porter stemmer to remove word stems (e.g. running - run)
 
+#Helper function to apply the text preprocessing in order. 
 def apply_all(par):
     return stem_words(remove_stop_words(clean_text(par)))
 
 ### LDA Topic Modeling
 def train_lda(data):
-    num_topics = 100
-    chunksize = 100
-#    tokens = [word for word in data["description"].split()]
-    dictionary = corpora.Dictionary(data["description"])
-    corpus = [dictionary.doc2bow(doc) for doc in data['description']]
+    num_topics = 100 #Number of topics in the chunk
+    chunksize = 100 #Chunk size
+    dictionary = corpora.Dictionary(data["description"]) #Creates a dictionary object based on the book description
+    corpus = [dictionary.doc2bow(doc) for doc in data['description']] #Creates corpus
     # low alpha means each document is only represented by a small number of topics, and vice versa
     # low eta means each topic is only represented by a small number of words, and vice versa
+    #Uses gensim to create an LDA model based on the dictionary and corpus created above. 
     lda = LdaModel(corpus=corpus, num_topics=num_topics, id2word=dictionary,
                    alpha=1e-2, eta=0.5e-2, chunksize=chunksize, minimum_probability=0.0, passes=2)
     return dictionary,corpus,lda
@@ -80,7 +81,7 @@ def get_most_similar_books(query,matrix,k=10):
     """
     sims = jensen_shannon(query,matrix) # list of jensen shannon distances
     most_sim_ids = sims.argsort()[:k]
-    most_similar_df = train[train.index.isin(most_sim_ids)]
+    most_similar_df = df[df.index.isin(most_sim_ids)]
     return most_similar_df # the top k positional index of the smallest Jensen Shannon distances
 
 ## Plotting a histogram of the topic distribution for random book
@@ -97,11 +98,17 @@ def plotDoc(distribution):
 
 #Similarity scoring
 def get_similarity(lda, point, dictionary):
-    new_bow = dictionary.doc2bow(test.iloc[point, 6])
+    new_bow = dictionary.doc2bow(df.iloc[point, 6])
     new_doc_distribution = np.array([tup[1] for tup in lda.get_document_topics(bow=new_bow)]) #tup[1] -> rating
     doc_topic_dist = np.array([[tup[1] for tup in lst] for lst in lda[corpus]])
     return get_most_similar_books(new_doc_distribution, doc_topic_dist)
 
+def display_image(link):
+    #downloads the image from the provided link
+    urllib.request.urlretrieve(link, "cover.png")
+    im = Image.open("cover.png") #opens the downloaded image
+    im = im.resize((500,800)) #resizes it for better visibility
+    im.show() #shows the image
 
 #creating a new dataframe object
 df = pd.read_csv("data.csv", index_col=0)
@@ -117,11 +124,40 @@ test = df[~point] #Reserves the rest of the data for testing
 dictionary, corpus, lda = train_lda(df)
 #trial = lda.show_topics(num_topics=10, num_words=20)
 #print(trial)
+print(df.head())
 
-
+name = ""
 try:
-    point = int(input("Enter a book index: "))
+    name = input("Enter a book name: ").lower()
 except:
-    print("Invalid index")
+    print("Name not found in library")
 
-print(get_similarity(lda, point, dictionary))
+print("You said: ", name)
+title = []
+for t in df["title"]:
+    if name in t:
+        title.append(t)
+
+#title = [t for t in df["title"] if t in name]
+#print(title)
+if len(title) > 1:
+   print("There were multiple books matching you're query: ")
+   [print(x) for x in title]
+   j = int(input("Which book did yu want? ")) - 1
+   print("You selected: ")
+elif len(title) == 0:
+    print("There are no titles matching your query. Please try again")
+    exit()
+else:
+   j = 0
+print(title[j])
+
+indx = int(df["title"].loc[lambda x: x==title[j]].index[0])
+
+res = get_similarity(lda, indx, dictionary)
+name = res["title"]
+img = res["image_url"]
+
+for item, url in zip(name, img):
+    print(item)
+    display_image(url)
